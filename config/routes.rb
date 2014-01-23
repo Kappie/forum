14b09b08b9c943beddf959c18b7732721b460f2b
Kappie <ghkapteijns@gmail.com>
2014-01-23 14:42:04 +0100

    added sign up form

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Forum::Application.routes.draw do
+  root "boards#index"
+  resources :users
+  get 'signup' => 'users#new', as: :signup
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
